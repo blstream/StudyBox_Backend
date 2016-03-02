dbd15573585daebe5d0863c7959bf9b59ffacad3
VanDavv <lukpila29@gmail.com>
2016-03-02 14:59:08 +0100

    Added couple of parameters to IdentifiableEntity

--- backend/app/src/main/java/com/bls/patronage/model/IdentifiableEntity.java
+++ backend/app/src/main/java/com/bls/patronage/model/IdentifiableEntity.java
@@ -5,9 +5,18 @@ import java.util.UUID;
 public class IdentifiableEntity {
     private final UUID id;
 
+    public IdentifiableEntity() {
+        this.id = null;
+    }
+
+    public IdentifiableEntity(String id) {
+        this.id = UUID.fromString(id);
+    }
+
     public IdentifiableEntity(UUID id) {
         this.id = id;
     }
+
 
     public UUID getId() {
         return id;
