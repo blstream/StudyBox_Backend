622afb411e6b38e40ee1126ec2702beeb996d011
VanDavv <lukpila29@gmail.com>
2016-03-02 14:59:34 +0100

    Created tests for IdentifiableEntity

--- backend/app/src/test/java/com/bls/patronage/ApplicationTest.java
+++ backend/app/src/test/java/com/bls/patronage/ApplicationTest.java
@@ -1,4 +1,35 @@
 package com.bls.patronage;
 
+import com.bls.patronage.model.IdentifiableEntity;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ApplicationTest {
+
+    private String testUUID;
+
+    @Before
+    public void setup() {
+        testUUID = "12345678-9012-3456-7890-123456789012";
+    }
+
+    @Test
+    public void creatingIdentifiableEntity() {
+        assertThat(
+                new IdentifiableEntity(
+                        UUID.fromString(testUUID)
+                ).getId()
+        ).isEqualToComparingFieldByField(
+                UUID.fromString(testUUID)
+        );
+    }
+
+    @Test
+    public void creatingIdentifiableEntityWithoutParameters() {
+        assertThat(new IdentifiableEntity().getId()).isNull();
+    }
 }
