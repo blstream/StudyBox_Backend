8539b82c86eff53acb6116f47916761040a6af3c
tjarosz <jarostom@gmail.com>
2016-03-06 12:33:34 +0100

    DecksResource updated, needed import added

--- backend/app/src/main/java/com/bls/patronage/resources/DecksResource.java
+++ backend/app/src/main/java/com/bls/patronage/resources/DecksResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.WebApplicationException;
 
 import com.bls.patronage.api.DeckRepresentation;
 import com.bls.patronage.db.dao.DeckDAO;
@@ -26,7 +27,7 @@ public class DecksResource {
 
     @POST
     public Deck createDeck(DeckRepresentation deck) {
-        if (deck.getName().isEmpty())
+        if(deck.getName().isEmpty())
             throw new WebApplicationException(400);
         Deck createdDeck = new Deck(UUID.randomUUID(), deck.getName());
         decksDAO.createDeck(createdDeck);
