4a02ab1d5c2600c7528bde3cef84122cc4abfa69
VanDavv <lukpila29@gmail.com>
2016-03-16 10:23:42 +0100

    Cleanup #2

--- backend/app/src/main/java/com/bls/patronage/resources/FlashcardsResource.java
+++ backend/app/src/main/java/com/bls/patronage/resources/FlashcardsResource.java
@@ -6,7 +6,12 @@ import com.bls.patronage.db.model.Flashcard;
 import io.dropwizard.jersey.params.UUIDParam;
 
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.UUID;
