2ba7b42da1fbc9bc180897cba2ebe73bf5e67c2f
VanDavv <lukpila29@gmail.com>
2016-03-18 10:08:12 +0100

    Adding ResultMapper

---
+++ backend/db/src/main/java/com/bls/patronage/db/dao/ResultMapper.java
@@ -0,0 +1,16 @@
+package com.bls.patronage.db.dao;
+
+import com.bls.patronage.db.model.Result;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class ResultMapper implements ResultSetMapper<Result> {
+
+    public Result map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        return new Result((UUID) r.getObject("flashcardId"), r.getInt("correctAnswers"));
+    }
+}
