062e5c51c9591810813ae59d961c6bd9c17ae08c
VanDavv <lukpila29@gmail.com>
2016-03-20 16:52:50 +0100

    Adding UserWithoutPasswordMapper

---
+++ backend/db/src/main/java/com/bls/patronage/db/mappers/UserWithoutPasswordMapper.java
@@ -0,0 +1,16 @@
+package com.bls.patronage.db.mappers;
+
+import com.bls.patronage.db.model.UserWithoutPassword;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class UserWithoutPasswordMapper implements ResultSetMapper<UserWithoutPassword> {
+    @Override
+    public UserWithoutPassword map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        return new UserWithoutPassword((UUID) r.getObject("id"), r.getString("email"), r.getString("name"));
+    }
+}
