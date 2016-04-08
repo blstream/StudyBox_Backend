5ba18f3897b0d1509d5b655142ef8c9fc50736ce
VanDavv <lukpila29@gmail.com>
2016-04-08 07:46:29 +0200

    Adding basic user

--- backend/app/src/main/java/com/bls/patronage/StudyBox.java
+++ backend/app/src/main/java/com/bls/patronage/StudyBox.java
@@ -7,14 +7,14 @@ import com.bls.patronage.db.dao.TipDAO;
 import com.bls.patronage.db.dao.UserDAO;
 import com.bls.patronage.db.exception.DataAccessExceptionMapper;
 import com.bls.patronage.db.model.User;
-import com.bls.patronage.resources.FlashcardResource;
-import com.bls.patronage.resources.FlashcardsResource;
-import com.bls.patronage.resources.UserResource;
-import com.bls.patronage.resources.UsersResource;
-import com.bls.patronage.resources.TipResource;
-import com.bls.patronage.resources.TipsResource;
 import com.bls.patronage.resources.DeckResource;
 import com.bls.patronage.resources.DecksResource;
+import com.bls.patronage.resources.FlashcardResource;
+import com.bls.patronage.resources.FlashcardsResource;
+import com.bls.patronage.resources.TipResource;
+import com.bls.patronage.resources.TipsResource;
+import com.bls.patronage.resources.UserResource;
+import com.bls.patronage.resources.UsersResource;
 import io.dropwizard.Application;
 import io.dropwizard.auth.AuthDynamicFeature;
 import io.dropwizard.auth.AuthValueFactoryProvider;
@@ -28,8 +28,6 @@ import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.skife.jdbi.v2.DBI;
-
-import static com.bls.patronage.StudyBoxConfiguration.isAuthenticationEnabled;
 
 public class StudyBox extends Application<StudyBoxConfiguration> {
 
@@ -79,7 +77,7 @@ public class StudyBox extends Application<StudyBoxConfiguration> {
         final BasicAuthenticator basicAuthenticator = new BasicAuthenticator(jdbi.onDemand(UserDAO.class));
         final CachingAuthenticator cachingAuthenticator = new CachingAuthenticator(environment.metrics(), basicAuthenticator,
                 configuration.getAuthCacheBuilder());
-        if(isAuthenticationEnabled) {
+        if (configuration.getIsAuthenticationEnabled()) {
             environment.jersey().register(new AuthDynamicFeature(
                     new BasicCredentialAuthFilter.Builder<User>()
                             .setAuthenticator(cachingAuthenticator)
--- backend/app/src/main/java/com/bls/patronage/StudyBoxConfiguration.java
+++ backend/app/src/main/java/com/bls/patronage/StudyBoxConfiguration.java
@@ -1,5 +1,6 @@
 package com.bls.patronage;
 
+import com.bls.patronage.db.model.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.cache.CacheBuilder;
 import io.dropwizard.Configuration;
@@ -10,13 +11,15 @@ import javax.validation.constraints.NotNull;
 
 public class StudyBoxConfiguration extends Configuration {
     @NotNull
-    public static Boolean isAuthenticationEnabled;
     public static final int PW_HASH_SECURITY_LEVEL = 12;
     private static final String DEFAULT_AUTH_CACHE_SPEC = "maximumSize=1000,expireAfterAccess=1h";
-
+    public Boolean isAuthenticationEnabled;
     @Valid
     @NotNull
     private DataSourceFactory database = new DataSourceFactory();
+    @Valid
+    @NotNull
+    private User defaultUser;
 
     public DataSourceFactory getDatabase() {
         return database;
@@ -34,5 +37,15 @@ public class StudyBoxConfiguration extends Configuration {
 
     public CacheBuilder<Object, Object> getAuthCacheBuilder() {
         return CacheBuilder.from(DEFAULT_AUTH_CACHE_SPEC);
+    }
+
+    @JsonProperty
+    public User getDefaultUser() {
+        return this.defaultUser;
+    }
+
+    @JsonProperty
+    public void setDefaultUser(User defaultUser) {
+        this.defaultUser = defaultUser;
     }
 }
