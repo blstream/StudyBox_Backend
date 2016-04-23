c90130f4dc9e7f5888789ccbb77a55b610852b07
VanDavv <lukpila29@gmail.com>
2016-04-24 00:23:38 +0200

    Adding bundle to application

--- backend/app/src/main/java/com/bls/patronage/StudyBoxConfiguration.java
+++ backend/app/src/main/java/com/bls/patronage/StudyBoxConfiguration.java
@@ -1,18 +1,22 @@
 package com.bls.patronage;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 import com.google.common.cache.CacheBuilder;
-
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
 
 public class StudyBoxConfiguration extends Configuration {
 
     public static final int PW_HASH_SECURITY_LEVEL = 12;
 
     private static final String DEFAULT_AUTH_CACHE_SPEC = "maximumSize=1000,expireAfterAccess=1h";
+
+    @Valid
+    @NotNull
+    private URI cvServerURI;
 
     @Valid
     @NotNull
@@ -26,4 +30,7 @@ public class StudyBoxConfiguration extends Configuration {
         return CacheBuilder.from(DEFAULT_AUTH_CACHE_SPEC);
     }
 
+    public URI getCVServerURI() {
+        return cvServerURI;
+    }
 }
