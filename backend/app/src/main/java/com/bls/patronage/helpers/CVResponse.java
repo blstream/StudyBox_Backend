782cf9fb2697c5a2cc4ff77eac9c336a11d0527a
VanDavv <lukpila29@gmail.com>
2016-05-09 21:46:58 +0200

    Adding classes to handle CV communication messages

---
+++ backend/app/src/main/java/com/bls/patronage/helpers/CVResponse.java
@@ -0,0 +1,40 @@
+package com.bls.patronage.helpers;
+
+import com.bls.patronage.api.FlashcardRepresentation;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class CVResponse {
+    @JsonProperty
+    public List<FlashcardRepresentation> flashcards;
+    @JsonProperty
+    public Integer status;
+
+    public CVResponse() {
+        //Jackson deserialization
+    }
+
+    public CVResponse(List<FlashcardRepresentation> flashcards, Integer status) {
+        this.flashcards = flashcards;
+        this.status = status;
+    }
+
+    public List<FlashcardRepresentation> getFlashcards() {
+        return flashcards;
+    }
+
+    public CVResponse setFlashcards(List<FlashcardRepresentation> flashcards) {
+        this.flashcards = flashcards;
+        return this;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public CVResponse setStatus(Integer status) {
+        this.status = status;
+        return this;
+    }
+}
