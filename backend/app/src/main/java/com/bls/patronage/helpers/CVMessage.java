78b287d4cc6c5c76550654dad4031d76adff7e13
VanDavv <lukpila29@gmail.com>
2016-05-09 21:46:58 +0200

    Adding classes to handle CV communication messages

--- backend/app/src/main/java/com/bls/patronage/helpers/CVMessage.java
+++ backend/app/src/main/java/com/bls/patronage/helpers/CVMessage.java
@@ -1,16 +1,27 @@
 package com.bls.patronage.helpers;
 
 import com.bls.patronage.Message;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.net.URL;
+import java.net.URI;
 
 public class CVMessage implements Message {
 
-    private final URL location;
-    private final String action;
+    public final URI location;
+    public final String action;
 
-    public CVMessage(URL location, String action) {
+    public CVMessage(URI location, String action) {
         this.location = location;
         this.action = action;
+    }
+
+    @JsonProperty
+    public URI getLocation() {
+        return location;
+    }
+
+    @JsonProperty
+    public String getAction() {
+        return action;
     }
 }
