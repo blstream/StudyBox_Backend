5a4e1664dd588978214d2883b2b6edb0d3682e62
tjarosz <jarostom@gmail.com>
2016-05-10 18:01:47 +0200

    added updatePassord method

--- backend/app/src/main/java/com/bls/patronage/resources/ResetPasswordResource.java
+++ backend/app/src/main/java/com/bls/patronage/resources/ResetPasswordResource.java
@@ -5,6 +5,7 @@ import com.bls.patronage.api.PasswordChangeRepresentation;
 import com.bls.patronage.db.dao.TokenDAO;
 import com.bls.patronage.db.dao.UserDAO;
 import com.bls.patronage.db.model.ResetPasswordToken;
+import com.bls.patronage.db.model.User;
 import com.bls.patronage.service.ResetPasswordService;
 import com.bls.patronage.service.TokenService;
 
@@ -16,6 +17,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
+
+import static com.bls.patronage.auth.BasicAuthenticator.generateSafeHash;
 
 @Path("/users/password")
 @Produces(MediaType.APPLICATION_JSON)
@@ -46,10 +49,14 @@ public class ResetPasswordResource {
 
     @POST
     @Path("/change")
-    public Response changePassword(@Valid PasswordChangeRepresentation user) {
+    public Response changePassword(@Valid PasswordChangeRepresentation userInfo) {
 
-        tokenDAO.getToken(user.getToken());
-        tokenDAO.deactivate(user.getToken());
+        tokenDAO.getToken(userInfo.getToken());
+        final User user = userDAO.getUserByEmail(userInfo.getEmail());
+        userDAO.updatePassword(new User(user.getId(), user.getEmail(), user.getName(),
+                generateSafeHash(userInfo.getPassword())));
+
+        tokenDAO.deactivate(userInfo.getToken());
 
         return Response.ok().status(Response.Status.OK).build();
     }
