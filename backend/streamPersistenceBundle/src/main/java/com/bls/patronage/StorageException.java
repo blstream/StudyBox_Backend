20e7f4315cb5dd12bf95ad74e7d5835f9920f299
VanDavv <lukpila29@gmail.com>
2016-05-11 20:46:46 +0200

    Changing return type and documenting exception class

--- backend/streamPersistenceBundle/src/main/java/com/bls/patronage/StorageException.java
+++ backend/streamPersistenceBundle/src/main/java/com/bls/patronage/StorageException.java
@@ -2,6 +2,10 @@ package com.bls.patronage;
 
 import java.io.IOException;
 
+/**
+ * This exception is thrown within streamPersistenceBundle.
+ * It's used to separate errors thrown in this bundle from other exceptions
+ */
 public class StorageException extends IOException {
     public StorageException() {
     }
