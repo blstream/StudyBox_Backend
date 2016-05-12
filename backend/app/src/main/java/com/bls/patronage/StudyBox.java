24dd812abd26121a72362d2c879982566d1e9571
Arek <arek314arek@gmail.com>
2016-05-12 13:37:30 +0200

    modified:   backend/app/src/main/java/com/bls/patronage/StudyBox.java 	modified:   backend/app/src/test/java/com/bls/patronage/resources/BasicAuthenticationTest.java

--- backend/app/src/main/java/com/bls/patronage/StudyBox.java
+++ backend/app/src/main/java/com/bls/patronage/StudyBox.java
@@ -2,44 +2,11 @@ package com.bls.patronage;
 
 import com.bls.patronage.auth.BasicAuthenticator;
 import com.bls.patronage.auth.PreAuthenticationFilter;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import com.bls.patronage.db.dao.AuditDAO;
-=======
->>>>>>> parent of 766818e... 	modified:   backend/app/src/main/java/com/bls/patronage/StudyBox.java
-=======
->>>>>>> parent of 766818e... 	modified:   backend/app/src/main/java/com/bls/patronage/StudyBox.java
 import com.bls.patronage.db.dao.DeckDAO;
 import com.bls.patronage.db.dao.FlashcardDAO;
 import com.bls.patronage.db.dao.ResultDAO;
 import com.bls.patronage.db.dao.TipDAO;
 import com.bls.patronage.db.dao.UserDAO;
-<<<<<<< HEAD
-<<<<<<< HEAD
-import com.bls.patronage.db.model.User;
-import com.bls.patronage.mapper.DataAccessExceptionMapper;
-import com.bls.patronage.resources.AuditsResource;
-=======
-import com.bls.patronage.mapper.DataAccessExceptionMapper;
-import com.bls.patronage.db.model.User;
->>>>>>> parent of 766818e... 	modified:   backend/app/src/main/java/com/bls/patronage/StudyBox.java
-import com.bls.patronage.resources.DeckResource;
-import com.bls.patronage.resources.DecksResource;
-import com.bls.patronage.resources.FlashcardResource;
-import com.bls.patronage.resources.FlashcardsResource;
-import com.bls.patronage.resources.ResultsResource;
-import com.bls.patronage.resources.TipResource;
-import com.bls.patronage.resources.TipsResource;
-import com.bls.patronage.resources.UserResource;
-import com.bls.patronage.resources.UsersResource;
-=======
-import com.bls.patronage.db.dao.*;
-import com.bls.patronage.mapper.DataAccessExceptionMapper;
-import com.bls.patronage.db.model.User;
-import com.bls.patronage.resources.*;
->>>>>>> parent of 9454ad7... 	modified:   backend/app/src/main/java/com/bls/patronage/StudyBox.java
-=======
 import com.bls.patronage.mapper.DataAccessExceptionMapper;
 import com.bls.patronage.db.model.User;
 import com.bls.patronage.resources.DeckResource;
@@ -51,7 +18,6 @@ import com.bls.patronage.resources.TipResource;
 import com.bls.patronage.resources.TipsResource;
 import com.bls.patronage.resources.UserResource;
 import com.bls.patronage.resources.UsersResource;
->>>>>>> parent of 766818e... 	modified:   backend/app/src/main/java/com/bls/patronage/StudyBox.java
 import io.dropwizard.Application;
 import io.dropwizard.auth.AuthDynamicFeature;
 import io.dropwizard.auth.AuthValueFactoryProvider;
