5daf18c5c58255bf9982302a7671ba862eb1b542
VanDavv <lukpila29@gmail.com>
2016-05-22 00:02:36 +0200

    renaming from location to url in CVRequest

--- backend/app/src/main/java/com/bls/patronage/cv/CVRequest.java
+++ backend/app/src/main/java/com/bls/patronage/cv/CVRequest.java
@@ -1,34 +1,43 @@
 package com.bls.patronage.cv;
 
-import com.bls.patronage.api.AcceptableFileTypes;
+import com.bls.patronage.api.AcceptableFileType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.net.URI;
+import java.net.URL;
 
 public class CVRequest {
 
-    public final URI location;
+    public final URL url;
     public final String action;
 
-    public CVRequest(URI location, String action) {
-        this.location = location;
+    public CVRequest(URL url, String action) {
+        this.url = url;
         this.action = action;
     }
 
-    public static CVRequest createRecognizeRequest(final URI publicURLToUploadedFile, String fileType) {
+    public static CVRequest createRecognizeRequest(final URL publicURLToUploadedFile, String fileType) {
+        System.out.println(fileType);
         return new CVRequest(
                 publicURLToUploadedFile,
-                fileType.equals(AcceptableFileTypes.IMAGE) ? "ImageToFlashcard" : "TextToFlashcard"
+                fileType.equals(AcceptableFileType.IMAGE.toString()) ? "ImageToFlashcard" : "TextToFlashcard"
         );
     }
 
     @JsonProperty
-    public URI getLocation() {
-        return location;
+    public URL getUrl() {
+        return url;
     }
 
     @JsonProperty
     public String getAction() {
         return action;
+    }
+
+    @Override
+    public String toString() {
+        return "CVRequest{" +
+                "url=" + url +
+                ", action='" + action + '\'' +
+                '}';
     }
 }
