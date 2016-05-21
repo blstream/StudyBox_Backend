025c02c553ada65ccb4e683f6b7991a5f9c76197
VanDavv <lukpila29@gmail.com>
2016-05-22 00:03:19 +0200

    adding missing property in CVResponse

--- backend/app/src/main/java/com/bls/patronage/cv/CVResponse.java
+++ backend/app/src/main/java/com/bls/patronage/cv/CVResponse.java
@@ -1,15 +1,19 @@
 package com.bls.patronage.cv;
 
 import com.bls.patronage.api.FlashcardRepresentation;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CVResponse {
     @JsonProperty
     public List<FlashcardRepresentation> flashcards;
     @JsonProperty
     public Integer status;
+    @JsonProperty
+    public String error_description;
 
     public CVResponse() {
         //Jackson deserialization
@@ -18,6 +22,17 @@ public class CVResponse {
     public CVResponse(List<FlashcardRepresentation> flashcards, Integer status) {
         this.flashcards = flashcards;
         this.status = status;
+    }
+
+    public CVResponse(Integer status, String error_description) {
+        this.status = status;
+        this.error_description = error_description;
+    }
+
+    public CVResponse(List<FlashcardRepresentation> flashcards, Integer status, String error_description) {
+        this.flashcards = flashcards;
+        this.status = status;
+        this.error_description = error_description;
     }
 
     public List<FlashcardRepresentation> getFlashcards() {
@@ -36,6 +51,24 @@ public class CVResponse {
     public CVResponse setStatus(Integer status) {
         this.status = status;
         return this;
+    }
+
+    public String getError_description() {
+        return error_description;
+    }
+
+    public CVResponse setError_description(String error_description) {
+        this.error_description = error_description;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "CVResponse{" +
+                "flashcards=" + flashcards +
+                ", status=" + status +
+                ", error_description='" + error_description + '\'' +
+                '}';
     }
 }
 
