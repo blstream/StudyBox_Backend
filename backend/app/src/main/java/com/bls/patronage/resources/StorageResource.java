f8666e5ec010732f1a8951cb812aa2922892b452
VanDavv <lukpila29@gmail.com>
2016-05-27 12:41:44 +0200

    Removing new dependency

--- backend/app/src/main/java/com/bls/patronage/resources/StorageResource.java
+++ backend/app/src/main/java/com/bls/patronage/resources/StorageResource.java
@@ -5,7 +5,6 @@ import com.bls.patronage.StorageException;
 import com.bls.patronage.StorageService;
 import com.bls.patronage.db.model.User;
 import io.dropwizard.auth.Auth;
-import org.apache.commons.io.IOUtils;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,7 +13,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 @Path("/storage/{userId: [0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}}/{context}/{storageId: [0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}}")
@@ -27,6 +28,23 @@ public class StorageResource {
         this.storageService = storageService;
     }
 
+    private static void copyStream(final InputStream input, final OutputStream output) throws StorageException {
+        final int EOF = -1;
+        final int BUFFER_SIZE = 1024 * 4;
+        final byte[] BUFFER = new byte[BUFFER_SIZE];
+
+        int n;
+        try {
+            while (EOF != (n = input.read(BUFFER))) {
+                output.write(BUFFER, 0, n);
+            }
+        } catch (IOException e) {
+            throw new StorageException(e);
+        }
+
+
+    }
+
     @GET
     public StreamingOutput getFile(@Auth User user,
                                    @PathParam("userId") UUID userId,
@@ -37,7 +55,7 @@ public class StorageResource {
 
         return os -> {
             final InputStream fileStream = storageService.get(userId, StorageContexts.CV, storageId);
-            IOUtils.copy(fileStream, os);
+            copyStream(fileStream, os);
             os.flush();
         };
     }
